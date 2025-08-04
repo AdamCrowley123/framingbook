b32d04d4b5005ab0ee59080a93117f7779d04559
AdamCrowley123 <ciccwiu@hotmail.it>
2025-08-04 15:17:39 +0200

    topolino fix

--- constants.ts
+++ constants.ts
@@ -11,6 +11,6 @@ export const PAGE_SIZE_OPTIONS: PageSize[] = [
   { name: 'Manga Tankōbon', subtitle: 'B6', width: 1512, height: 2150, backgroundImage: '/tankobon.png' },
   { name: 'Manga Deluxe', subtitle: 'A5', width: 1748, height: 2480, backgroundImage: '/deluxe.png' },
   { name: 'US Comic Book', subtitle: 'Standard', width: 1988, height: 3075, backgroundImage: '/uscomic.png' },
-  { name: 'Topolino Libretto', subtitle: 'Digest', width: 1476, height: 2185, backgroundImage: '/topolino.png' },
+  { name: 'Topolino Libretto', subtitle: 'Digest', width: 1737, height: 2319, backgroundImage: '/topolino.png' },
   { name: 'Franco-Belga', subtitle: 'Album', width: 2550, height: 3366, backgroundImage: '/francobelga.png' },
 ];
